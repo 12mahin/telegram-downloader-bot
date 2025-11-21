19afa2db7aba865d6c74a3d44abd83c066dbb78d
12mahin <105595894+12mahin@users.noreply.github.com>
2025-11-21 22:17:11 +0600

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-API_ID = 30863103               # আপনার Telegram API ID
-API_HASH = "7445ed597b3cae418f1975aec11895ef"      # Telegram API Hash
-BOT_TOKEN = "7858742184:AAHc37UjI47pU2A-iD2LMAjm5C-KxZkmr3w"    # BotFather থেকে পাওয়া Bot Token
-CHANNEL_ID =  7858742184     # Auto-upload Channel ID
